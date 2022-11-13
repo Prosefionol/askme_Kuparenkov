e298d2be13742e08a6e2a95df5e4dccc4b311d71
Prosefionol <ilya_kuper@mail.ru>
2022-11-13 04:41:01 +0300

    homework p2 without pagination

--- app/views.py
+++ app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from . import models
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {'questions': models.QUESTIONS}
+    return render(request, 'index.html', context=context)
 
 
 def question(request, question_id: int):
-    return render(request, 'question.html')
+    if question_id >= len(models.QUESTIONS):
+        context_a = {'maxsize': len(models.QUESTIONS)}
+        return render(request, 'error.html', context=context_a)
+    else:
+        question_item = models.QUESTIONS[question_id]
+        context_b = {'question': question_item, 'answers': models.ANSWERS}
+        return render(request, 'question.html', context=context_b)
 
 
 def ask(request):
